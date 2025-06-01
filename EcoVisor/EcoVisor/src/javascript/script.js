61fffe340e39700c9aeec670023bd6bedc32ecf6
Brendha Amorim <111456575+Brdh@users.noreply.github.com>
2025-06-01 14:17:08 -0300

    Add files via upload

---
+++ EcoVisor/EcoVisor/src/javascript/script.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+    const sections = $('section');
+    const navItems = $('.nav-item');
+    const header = $('header');
+
+    // ✅ BOTÃO MENU MOBILE
+    $('#mobile_btn').on('click', function () {
+        $('#mobile_menu').toggleClass('active');
+        $(this).find('i').toggleClass('fa-x');
+    });
+
+    function updateActiveNav() {
+        const scrollPosition = $(window).scrollTop() + header.outerHeight();
+        let activeSectionIndex = 0;
+
+        sections.each(function (i) {
+            const section = $(this);
+            const sectionTop = section.offset().top;
+            const sectionBottom = sectionTop + section.outerHeight();
+
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                activeSectionIndex = i;
+                return false;
+            }
+        });
+
+        navItems.removeClass('active');
+        $(navItems[activeSectionIndex]).addClass('active');
+    }
+
+   
+    $(window).on('scroll', function () {
+        const scrollPosition = $(window).scrollTop();
+
+        if (scrollPosition <= 0) {
+            header.css('box-shadow', 'none');
+        } else {
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)'); // 👈 corrigido parêntese
+        }
+
+        updateActiveNav();
+    });
+
+   
+    $('.nav-item a').on('click', function () {
+        setTimeout(updateActiveNav, 100);
+    });
+
+    ScrollReveal().reveal('#cta', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('.extinto', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('#desmatamento', {
+        origin: 'left',
+        duration: 1000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('.feedback', {
+        origin: 'right',
+        duration: 1000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('.reveal-onca', {
+        delay: 300,
+        duration: 1000,
+        origin: 'right',
+        distance: '50px',
+        easing: 'ease-in-out'
+    });
+});
